7a19ac753181957d5821dfe9538b7753f2f5f94a
100ask <user@100ask.com>
2024-07-09 09:35:04 -0400

    initial commit

---
+++ build/src/CMakeFiles/mprpc.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/book/projects/mprpc/src/logger.cc" "/home/book/projects/mprpc/build/src/CMakeFiles/mprpc.dir/logger.cc.o"
+  "/home/book/projects/mprpc/src/mprpcapplication.cc" "/home/book/projects/mprpc/build/src/CMakeFiles/mprpc.dir/mprpcapplication.cc.o"
+  "/home/book/projects/mprpc/src/mprpcchannel.cc" "/home/book/projects/mprpc/build/src/CMakeFiles/mprpc.dir/mprpcchannel.cc.o"
+  "/home/book/projects/mprpc/src/mprpcconfig.cc" "/home/book/projects/mprpc/build/src/CMakeFiles/mprpc.dir/mprpcconfig.cc.o"
+  "/home/book/projects/mprpc/src/mprpccontroller.cc" "/home/book/projects/mprpc/build/src/CMakeFiles/mprpc.dir/mprpccontroller.cc.o"
+  "/home/book/projects/mprpc/src/rpcheader.pb.cc" "/home/book/projects/mprpc/build/src/CMakeFiles/mprpc.dir/rpcheader.pb.cc.o"
+  "/home/book/projects/mprpc/src/rpcprovider.cc" "/home/book/projects/mprpc/build/src/CMakeFiles/mprpc.dir/rpcprovider.cc.o"
+  "/home/book/projects/mprpc/src/zookeeperutil.cc" "/home/book/projects/mprpc/build/src/CMakeFiles/mprpc.dir/zookeeperutil.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/include"
+  "../example"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
